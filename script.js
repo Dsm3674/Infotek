3a9c7548312055504a80efe9971f2c76fe7b6b61
Divyanshu <divyanshusomasekhar1@gmail.com>
2025-12-29 09:15:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+  const main = document.getElementById("main-content");
+
+ 
   setTimeout(() => {
-    document.getElementById("loader").classList.add("hidden");
-    document.getElementById("main-content").classList.add("visible");
-  }, 800);
+    loader.classList.add("fade-out");
+  }, 600);
+
+ 
+  setTimeout(() => {
+    loader.remove();
+    main.classList.add("visible");
+  }, 1200);
 });
+
 
 const buttons = document.querySelectorAll("[data-menu]");
 const panels = document.querySelectorAll(".mega-panel");
 
 buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener("click", e => {
+    e.stopPropagation();
+
     const target = btn.dataset.menu;
     const panel = document.querySelector(`[data-panel="${target}"]`);
     const isOpen = panel.classList.contains("active");
@@ -24,7 +36,7 @@ buttons.forEach(btn => {
   });
 });
 
-// Close mega menu when clicking outside
+
 document.addEventListener("click", e => {
   if (!e.target.closest(".nav-center") && !e.target.closest(".mega-menu")) {
     panels.forEach(p => p.classList.remove("active"));
